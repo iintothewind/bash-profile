56547ac8dc30e02f926b2bc043673a75975ae8b5
yinghchen <yinghchen@paypal.com>
2020-06-05 20:09:38 +0800

    updated cf.sh

--- .shell/cf.sh
+++ .shell/cf.sh
@@ -51,35 +51,39 @@ function cf_rand() {
 #   - hdid(8)
 #
 function cf_mount_ram() {
-  mount_point=${1}
-  size=${2:-64}
+  if [[ $(uname) == Darwin ]]; then
+    mount_point=${1}
+    size=${2:-64}
 
-  mkdir -p $mount_point
-  if [ $? -ne 0 ]; then
-      echo "The mount point didn't available." >&2
-      return $?
+    mkdir -p $mount_point
+    if [ $? -ne 0 ]; then
+        echo "The mount point didn't available." >&2
+        return $?
+    fi
+
+    sector=$(expr $size \* 1024 \* 1024 / 512)
+    device_name=$(hdid -nomount "ram://${sector}" | awk '{print $1}')
+    if [ $? -ne 0 ]; then
+        echo "Could not create disk image." >&2
+        return $?
+    fi
+
+    newfs_hfs $device_name > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Could not format disk image." >&2
+        return $?
+    fi
+
+    mount -t hfs $device_name $mount_point
+    if [ $? -ne 0 ]; then
+        echo "Could not mount disk image." >&2
+        return $?
+    fi
+    return 0
+  else
+    echo "this function works on mac only" >&2
+    return 1
   fi
-
-  sector=$(expr $size \* 1024 \* 1024 / 512)
-  device_name=$(hdid -nomount "ram://${sector}" | awk '{print $1}')
-  if [ $? -ne 0 ]; then
-      echo "Could not create disk image." >&2
-      return $?
-  fi
-
-  newfs_hfs $device_name > /dev/null
-  if [ $? -ne 0 ]; then
-      echo "Could not format disk image." >&2
-      return $?
-  fi
-
-  mount -t hfs $device_name $mount_point
-  if [ $? -ne 0 ]; then
-      echo "Could not mount disk image." >&2
-      return $?
-  fi
-
-  return 0
 }
 
 # Usage:
@@ -92,25 +96,30 @@ function cf_mount_ram() {
 #   - hdid(8)
 #
 function cf_unmount_ram() {
-  mount_point=$1
-  if [ ! -d "${mount_point}" ]; then
-      echo "The mount point didn't available." >&2
-      return 1
-  fi
-  mount_point=$(cd $mount_point && pwd)
+  if [[ $(uname) == Darwin ]]; then
+    mount_point=$1
+    if [ ! -d "${mount_point}" ]; then
+        echo "The mount point didn't available." >&2
+        return 1
+    fi
+    mount_point=$(cd $mount_point && pwd)
 
-  device_name=$(df "${mount_point}" 2>/dev/null | tail -1 | grep "${mount_point}" | cut -d' ' -f1)
-  if [ -z "${device_name}" ]; then
-      echo "The mount point didn't mount disk image." >&2
-      return 1
-  fi
+    device_name=$(df "${mount_point}" 2>/dev/null | tail -1 | grep "${mount_point}" | cut -d' ' -f1)
+    if [ -z "${device_name}" ]; then
+        echo "The mount point didn't mount disk image." >&2
+        return 1
+    fi
 
-  umount "${mount_point}"
-  if [ $? -ne 0 ]; then
-      echo "Could not unmount." >&2
-      return $?
-  fi
+    umount "${mount_point}"
+    if [ $? -ne 0 ]; then
+        echo "Could not unmount." >&2
+        return $?
+    fi
 
-  hdiutil detach -quiet $device_name
-  return $?
+    hdiutil detach -quiet $device_name
+    return $?
+  else
+    echo "this function works on mac only" >&2
+    return 1
+  fi
 }
