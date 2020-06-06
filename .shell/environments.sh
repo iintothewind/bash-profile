745cc30a6982596d0ded0c8a0f9b6b7d8dc02a49
iintothewind <ivarchen@live.cn>
2020-06-07 00:01:04 +0800

    updated nvm

--- .shell/environments.sh
+++ .shell/environments.sh
@@ -127,6 +127,11 @@ if [[ $(uname) == Darwin ]]; then
     export PATH="$PATH:$HOME/.npm/modules/bin"
   fi
 
+  if test -s "$BREW_PREFIX/nvm/nvm.sh"; then
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+    [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+  fi
 
   # gnuman
   if test -d $BREW_PREFIX/coreutils/libexec/gnuman; then
