063f258a5637ae9ec555439beb7c533b664d7f9e
ivar <ivar.chen@ulala.ca>
2025-02-14 12:36:07 -0800

    added bark.sh

---
+++ .shell/bark.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+if [[ -f $HOME/.shell/cf.sh ]]; then
+
+  function cf_bark_ivar() {
+    local url="https://api.day.app/4CS4ncYoRbEkdbtsecagFT"
+    local msgPrefix='{"level":"active","action":"none","badge":1,"isArchive":1,"group":"test","title":"bark message","body":'
+    local msgContent=$(echo "${1:-`cat`}" | cf_json_escape)
+    local msgSuffix='}'
+
+    echo "${msgPrefix}${msgContent}${msgSuffix}" | cf_req -l $url -m "POST" | cf_json_format
+  }
+
+  function cf_bark_ashley() {
+    local url="https://api.day.app/VzhjJbmaBZzz6T6bBoMD73"
+    local msgPrefix='{"level":"active","action":"none","badge":1,"isArchive":1,"group":"test","title":"bark message","body":'
+    local msgContent=$(echo "${1:-`cat`}" | cf_json_escape)
+    local msgSuffix='}'
+
+    echo "${msgPrefix}${msgContent}${msgSuffix}" | cf_req -l $url -m "POST" | cf_json_format
+  }
+
+fi
+
--- .shell/cf.sh
+++ .shell/cf.sh
@@ -1,17 +1,5 @@
 #! /usr/bin/env bash
 
-function cf_is_number() {
-  [[ $1 =~ ^[+-]?[0-9]+([.][0-9]+)?$ ]]
-}
-
-function cf_is_integer() {
-  [[ $1 =~ ^[+-]?[0-9]+$ ]]
-}
-
-function cf_trim() {
-  echo "$@" | xargs
-}
-
 function cf_lower() {
   echo "${1:-`cat`}" | tr '[:upper:]' '[:lower:]'
 }
@@ -24,6 +12,95 @@ function cf_starts_with() {
   local str=$1
   local pre=$2
   [[ "$str" ==  ${pre}* ]]
+}
+
+function cf_compose_url() {
+  local inputUrl=$1
+  local baseUrl=$2
+  if test $inputUrl && (cf_starts_with $inputUrl $baseUrl || [[ $inputUrl =~ http.?://.+ ]]); then
+    echo $inputUrl
+  else
+    echo "$baseUrl$inputUrl"
+  fi
+}
+
+# used after a pipe, for example: echo '{ "k": "v"}' | cf_jsonfmt
+function cf_json_format() {
+  if type python > /dev/null 2>&1; then
+    python -mjson.tool
+  else
+    echo "python is not found in PATH"
+    return 1
+  fi
+}
+
+function cf_json_escape() {
+  if type python > /dev/null 2>&1; then
+    python -c 'import sys, json; print(json.dumps(sys.stdin.read().strip()))'
+  else
+    echo "python is not found in PATH"
+    return 1
+  fi
+}
+
+# -u : user name, default value: $REQ_USER
+# -p : password, default value: $REQ_PWD
+# -l : request url, it can be full url (startsWith http://baseurl.com) or sub-path (/data/resource), default value: $REQ_BASE
+# -m : request method, default value: $REQ_METHOD
+# -d : request body, defaut read from stdin by cat
+function cf_req() {
+  local OPTIND=1
+  local u;local p;local l;local m;local d;
+  while getopts ":u:p:l:m:d:" o; do
+      case "${o}" in
+          u)  u=${OPTARG} ;;
+          p)  p=${OPTARG} ;;
+          l)  l=${OPTARG} ;;
+          m)  m=${OPTARG} ;;
+          d)  d=${OPTARG} ;;
+      esac
+  done
+  if type curl > /dev/null 2>&1; then
+    if (test $u || test $REQ_USER) && (test $p || test $REQ_PWD); then
+      echo $(curl -k -s -u ${u:-$REQ_USER}:${p:-$REQ_PWD} -X `cf_upper ${m:-$REQ_METHOD}` -H "Accept: application/json" -H "Content-Type: application/json" $(cf_compose_url $l $REQ_BASE) -d @<(if test "GET" = `cf_upper ${m:-$REQ_METHOD}` || test "DELETE" = `cf_upper ${m:-$REQ_METHOD}`; then echo ""; else echo ${d:-`cat`}; fi))
+    else
+      echo $(curl -k -s -X `cf_upper ${m:-$REQ_METHOD}` -H "Accept: application/json" -H "Content-Type: application/json" $(cf_compose_url $l $REQ_BASE) -d @<(if test "GET" = `cf_upper ${m:-$REQ_METHOD}` || test "DELETE" = `cf_upper ${m:-$REQ_METHOD}`; then echo ""; else echo ${d:-`cat`}; fi))
+    fi
+  else
+    echo "curl is not found in PATH"
+    return 1
+  fi
+}
+
+# cf_req -d '{ "query" : "MATCH (ee:Person) WHERE ee.name = \"Emil\" RETURN ee;", "params" : {} }'| cf_parse '["data"][0][0]["all_relationships"]'
+# $1 the key
+# $2 json string or read stdin from cat
+function cf_parse() {
+  if type python > /dev/null 2>&1; then
+    python <<EOF
+import json
+try:
+  print(json.loads('''${2:-`cat`}''')${1:-""})
+except Exception as e:
+  print("error: {}".format(e))
+EOF
+  else
+    echo "python is not found in PATH"
+    return 1
+  fi
+}
+
+
+function cf_is_number() {
+  [[ $1 =~ ^[+-]?[0-9]+([.][0-9]+)?$ ]]
+}
+
+function cf_is_integer() {
+  [[ $1 =~ ^[+-]?[0-9]+$ ]]
+}
+
+function cf_trim() {
+  echo "$@" | xargs
 }
 
 function cf_tail_args() {
@@ -134,7 +211,7 @@ function cf_convert_to_opus()  {
     else
       echo "path $path or extension $ext is invalid"
     fi
-  else 
+  else
     echo "ffmpeg or parallel is not installed"
   fi
 }
@@ -149,7 +226,7 @@ function cf_convert_to_orbis()  {
     else
       echo "path $path or extension $ext is invalid"
     fi
-  else 
+  else
     echo "ffmpeg or parallel is not installed"
   fi
 }
@@ -164,7 +241,7 @@ function cf_convert_to_m4a()  {
     else
       echo "path $path or extension $ext is invalid"
     fi
-  else 
+  else
     echo "ffmpeg or parallel is not installed"
   fi
 }
